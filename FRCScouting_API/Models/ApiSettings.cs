b50ef017cec6ec50d1bac388ebb423c694168571
Noah Tomkins <1tomkinsnoa@gmail.com>
2022-04-12 00:22:16 -0400

    Updated Program to add services and sql server

---
+++ FRCScouting_API/Models/ApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FRCScouting_API.Models
+{
+    public class ApiSettings
+    {
+        public string? ConfigurationDataContext { get; set; } // keep in appsettings.json
+        public string? ConfigurationDataContextCredentials { get; set; } // keep in secrets
+    }
+}
